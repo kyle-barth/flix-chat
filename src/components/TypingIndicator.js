dcd5832d69687b7211288e46ed48445b98c91b3b
VK <vitaliy.krynytskyy@gmail.com>
2018-06-15 23:45:29 +0100

    Changed typing indicator

--- src/components/TypingIndicator.js
+++ src/components/TypingIndicator.js
@@ -5,7 +5,7 @@ class TypingIndicator extends Component {
         if (this.props.usersWhoAreTyping.length > 0) {
             return (
                 <div>
-                    {`${this.props.usersWhoAreTyping.slice(0, 2).join(' and ')} is typing`}
+                    {`${this.props.usersWhoAreTyping.slice(0, 2).join(' and ')} is typing...`}
                 </div>
             );
         }
